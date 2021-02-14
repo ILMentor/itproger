01fe6002069e03db0c5a81e8484d884a334453c3
ILMentor <dekstroer8202@gmail.com>
2021-02-14 13:32:42 +0100

    Закончил четвертое видео.
    Создал базы данных. Тут, в самом проекте добавил переход от одной формы к другой при нажатии на кнопку.

--- src/sample/Controllers/SampleController.java
+++ src/sample/Controllers/SampleController.java
@@ -1,11 +1,16 @@
 package sample.Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class SampleController {
 
@@ -29,6 +34,22 @@ public class SampleController {
 
     @FXML
     void initialize() {
-        signInButton.setOnAction(actionEvent -> System.out.println("Text."));
+        signUpButton.setOnAction(actionEvent -> {
+            signUpButton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/Views/signUp.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
     }
 }
