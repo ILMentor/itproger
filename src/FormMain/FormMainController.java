4c3a6a104a437fe85bd2015a0f183bb258202d05
ILMentor <dekstroer8202@gmail.com>
2021-02-14 12:25:56 +0100

    Большой скачок в развитии проекта.
    Есть два пакета, один тренировочный, другой с настоящим проектом. Класс Main вынесен из пакетов в папку src. Прогресс тут это два видео из той серии гайдов. Создал второе окно. Добавил свой первый ресурс, теперь в моем проекте есть картинка.

---
+++ src/FormMain/FormMainController.java
@@ -0,0 +1,4 @@
+package FormMain;
+
+public class FormMainController {
+}
---
+++ src/sample/SampleController.java
@@ -0,0 +1,34 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SampleController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    void initialize() {
+        signInButton.setOnAction(actionEvent -> System.out.println("Text."));
+    }
+}
